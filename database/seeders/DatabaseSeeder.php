025135cf37333e7c0e761b2bce331c53198edebd
chronoll <kurotaka.wa@gmail.com>
2023-07-12 12:42:22 +0900

    seed to likes table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             PostSeeder::class,
             RelationshipSeeder::class,
+            LikeSeeder::class,
         ]);
     }
 }
