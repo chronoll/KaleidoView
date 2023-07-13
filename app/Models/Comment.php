0879dcefb6f5228646e43cf74e0284940ce919ce
chronoll <kurotaka.wa@gmail.com>
2023-07-13 22:15:12 +0900

    add relations to each model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+    
+    //親コメントへの参照
+    public function parent()
+    {
+        return $this->belongsTo(self::class,'parent_comment_id');
+    }
+    
+    //子コメントへの参照
+    public function children()
+    {
+        return $this->hasMany(self::class,'parent_comment_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,31 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+    
+    public function relationships()
+    {
+        return $this->hasMany(Relationship::class);
+    }
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+    
+    
 }
