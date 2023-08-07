33109d60b69f0aac00b43cbbb972959fa9c573b7
chronoll <kurotaka.wa@gmail.com>
2023-08-07 17:09:40 +0900

    カテゴリのユーザー情報を表示

--- resources/views/components/category-info.blade.php
+++ resources/views/components/category-info.blade.php
@@ -1,7 +1,11 @@
 <div>
     <div class='user'>
-        <div class='user_image'></div>
-        <div class='user_name'></div>
+        <div class='user_image'>
+            <img src="{{ $category->user->user_image }}" alt="画像が読み込めません。"/>
+        </div>
+        <div class='user_name'>
+            <p>{{'@'.$category->user->name}}</p>
+        </div>
     </div>
     <h1>{{$category->name}}</h1>
     <div class='followers'>
