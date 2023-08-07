777cecd6a69ae69bb2f81bd3b07bf23c38f97599
chronoll <kurotaka.wa@gmail.com>
2023-08-07 13:17:08 +0900

    コメント削除機能を追加

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -6,7 +6,13 @@
     
     <!-- 返信ボタンの追加 -->
     <button class="reply-button" data-comment-id="{{ $comment->id }}">返信</button>
-    
+    <form action='/comments/{{$comment->id}}' id='form_{{$comment->id}}'method='POST'>
+        @csrf
+        @method('DELETE')
+        @if(Auth::id()==$comment->user_id)
+        <button type='button' onclick='deleteComment({{$comment->id}})'>delete</button>
+        @endif
+    </form>
     @foreach($comment->children as $childComment)
         @include('comments', ['comment' => $childComment, 'margin' => $margin + 40])
     @endforeach
