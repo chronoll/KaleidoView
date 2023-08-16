e31ed632e87c7a48a909dda313dbfe30304220d6
chronoll <kurotaka.wa@gmail.com>
2023-08-16 22:51:21 +0900

    follow中のカテゴリを表示するサイドバーを設計

--- resources/views/posts/timeline.blade.php
+++ resources/views/posts/timeline.blade.php
@@ -2,21 +2,23 @@
     <x-slot name='header'>
         Timeline
     </x-slot>
-    <div class='categories'>
-        <h2>Following</h2>
-        @foreach($categories as $category)
-        <div class='category'>
-            <div class='user'>
-                <a href='{{route('users.show',$category->user->name)}}' style='text-decoration: none; color: inherit;'>
-                <img src='{{$category->user->user_image}}'/>
-                </a>
+    <div class='bg-gray-100 w-64 p-4 flex flex-col flex-shrink-0 md:w-50'>
+        <div class="sticky top-0 z-10">
+            <div class="text-2xl p-4 mb-4 flex justify-center items-center rounded-full">
+                Following
             </div>
-            <a href='{{route('categories.show',$category->id)}}' style='text-decoration: none; color: inherit;'>
-                <p class='category_name'>{{$category->name}}</p>
-                <p class='user_name'>{{$category->user->name}}</p>
+            @foreach($categories as $category)
+            <a href="{{route('categories.show',$category->id)}}">
+                <div class="h-24 bg-white rounded-lg shadow-lg ease-in transition duration-200 transform hover:scale-105 mb-4">
+                    <div class="h-12 w-full bg-gray-200 rounded-t-lg flex flex-col justify-between p-4 bg-cover bg-center" style="background-image: url('{{$category->category_image}}')"></div>
+                    <div class="flex items-center mb-2 space-x-4 px-2 pt-3 pb-1">
+                        <h1 class="text-gray-600">{{$category->name}}</h1>
+                        <p class="text-gray-400 text-sm">{{'@'.$category->user->name}}</p>
+                    </div>
+                </div>
             </a>
-        </div>
         @endforeach
+        </div>
     </div>
     <div class='posts'>
         @foreach ($posts as $post)
