4574c2ab568bf28894f03869341f05ccf25fa032
chronoll <kurotaka.wa@gmail.com>
2023-08-18 23:56:27 +0900

    edit_postni

--- resources/views/posts/edit_post.blade.php
+++ resources/views/posts/edit_post.blade.php
@@ -4,8 +4,6 @@
     </x-slot>
     <x-category-info :category='$category' />
     <div class='bg-gray-200 p-4'>
-    <h1>Edit your post</h1>
-    <p>{{$category->name}}</p>
     <form action='/posts/{{$post->id}}' method='POST'>
         @csrf
         @method('PUT')
@@ -44,3 +42,43 @@
         }
     }
 </script>
+<x-app-layout>
+    <x-slot name='header'>
+        <a href='{{route('categories.show',$category->id)}}'>>>{{$category->name}}</a>
+        <a href='{{route('posts.show',$post->id)}}'>>>{{$post->name}}</a>
+        <span> >>Edit Post</span>
+    </x-slot>
+    <x-category-info :category='$category' />
+    <div class='flex-1 bg-gray-200 p-4'>
+        <form action='/posts' method='POST' enctype='multipart/form-data'>
+            @csrf
+            <input type="hidden" name="post[category_id]" value="{{ $category->id }}" />
+            <div class='image'>
+                <div class="flex justify-between items-center">
+                    <input id='file-input' type='file' name='image' accept='image/*'>
+                    <button type='button' id='reset-file' class="px-4 py-2 bg-red-400 text-white rounded mr-4">Reset File</button>
+                </div>
+                <div class="relative bg-white m-4 mx-auto flex justify-center items-center" style="width: 400px; height: 400px;">
+                    <span class="text-center absolute z-0">画像を選択してください</span>
+                    <div id="crop-image-area" class="absolute top-0 left-0 w-full h-full flex justifys-center items-center bg-gray-300 z-10" style="display:none;">
+                        <div id="croppie" class="w-full max-w-screen-sm"></div>
+                        <button type="button" id="crop-button" class="absolute mt-3 px-4 py-2 bg-blue-600 text-white rounded" style="right: -150px;">Crop Image</button>
+                    </div>
+                    <div class="preview-cropped-image flex justify-center items-center h-full z-10"></div>
+                </div>
+                <input type="hidden" name="cropped_image" id="cropped_image">
+            </div>
+            <div class="mt-12 mb-8">
+                <label for="title" class="ml-2 text-sm block">タイトル</label>
+                <input id="title" type="text" name="post[title]" class="border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="タイトルを入力">
+            </div>
+            <div class="mb-8">
+                <label for="body" class="ml-2 text-sm block">本文</label>
+                <textarea id="body" name="post[body]"  class="h-40 border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="本文を入力"></textarea>
+            </div>
+            <div class="flex justify-end mt-4">
+                <input type='submit' value='投稿' class="px-4 py-2 bg-green-600 text-white rounded">
+                </div>
+        </form>
+    </div>
+</x-app-layout>
