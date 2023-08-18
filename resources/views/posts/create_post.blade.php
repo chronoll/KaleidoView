e4b8873df8dfa70d953a7c73f4ba45d3ca4866ab
chronoll <kurotaka.wa@gmail.com>
2023-08-18 23:45:57 +0900

    create_postビューにcssを適用

--- resources/views/posts/create_post.blade.php
+++ resources/views/posts/create_post.blade.php
@@ -1,45 +1,43 @@
 <x-app-layout>
     <x-slot name='header'>
-        New Post
+        <a href='{{route('categories.show',$category->id)}}'>{{$category->name}}</a>
+        <span> >>New Post</span>
     </x-slot>
     <x-category-info :category='$category' />
-    <div class='bg-gray-200 p-4'>
-        <h1>Create a new post</h1>
-        <p>{{$category->name}}</p>
+    <div class='flex-1 bg-gray-200 p-4'>
         <form action='/posts' method='POST' enctype='multipart/form-data'>
             @csrf
             <input type="hidden" name="post[category_id]" value="{{ $category->id }}" />
-            
             <div class='image'>
-                <input id='file-input' type='file' name='image' accept='image/*'>
-                <div id="crop-image-area" style="display:none;">
-                    <div id="croppie"></div>
-                    <button type="button" id="crop-button">Crop Image</button>
+                <div class="flex justify-between items-center">
+                    <input id='file-input' type='file' name='image' accept='image/*'>
+                    <button type='button' id='reset-file' class="px-4 py-2 bg-red-400 text-white rounded mr-4">Reset File</button>
                 </div>
-                <div class="preview-cropped-image"></div>
+                <div class="relative bg-white m-4 mx-auto flex justify-center items-center" style="width: 400px; height: 400px;">
+                    <span class="text-center absolute z-0">画像を選択してください</span>
+                    <div id="crop-image-area" class="absolute top-0 left-0 w-full h-full flex justifys-center items-center bg-gray-300 z-10" style="display:none;">
+                        <div id="croppie" class="w-full max-w-screen-sm"></div>
+                        <button type="button" id="crop-button" class="absolute mt-3 px-4 py-2 bg-blue-600 text-white rounded" style="right: -150px;">Crop Image</button>
+                    </div>
+                    <div class="preview-cropped-image flex justify-center items-center h-full z-10"></div>
+                </div>
                 <input type="hidden" name="cropped_image" id="cropped_image">
-                <button type='button' id='reset-file'>Reset File</button>
             </div>
-            <p class='content_error' style='color:red'>{{$errors->first('content')}}</p>
-            <div class='title'>
-                <h2>Title</h2>
-                <input type='text' name='post[title]' placeholder='タイトル'/>
+            <div class="mt-12 mb-8">
+                <label for="title" class="ml-2 text-sm block">タイトル</label>
+                <input id="title" type="text" name="post[title]" class="border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="タイトルを入力">
             </div>
-            
-            <div class='body'>
-                <h2>Body</h2>
-                <textarea name="post[body]" placeholder='本文を入力'></textarea>
+            <div class="mb-8">
+                <label for="body" class="ml-2 text-sm block">本文</label>
+                <textarea id="body" name="post[body]"  class="h-40 border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="本文を入力"></textarea>
             </div>
-            
-            <input type='submit' value='投稿'/>
+            <div class="flex justify-end mt-4">
+                <input type='submit' value='投稿' class="px-4 py-2 bg-green-600 text-white rounded">
+                </div>
         </form>
-        <div class='footer'>
-            <a href='{{route('categories.show',$category->id)}}'>戻る</a>
-        </div>
     </div>
 </x-app-layout>
 
-<!-- CSS and JS for Croppie -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js"></script>
 
@@ -65,13 +63,13 @@
             }
             croppieInstance = new Croppie(croppieElement, {
                 viewport: {
-                    width: 500,
-                    height: 500,
+                    width: 300,
+                    height: 300,
                     type: 'square'
                 },
                 boundary: {
-                    width: 700,
-                    height: 700
+                    width: 400,
+                    height: 400
                 },
                 enforceBoundary: true
             });
