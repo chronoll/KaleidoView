dc678173eaddefcdd24e606c19ace486b7003bba
chronoll <kurotaka.wa@gmail.com>
2023-08-19 18:48:53 +0900

    showビューの投稿部分のcssを変更

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,42 +3,57 @@
         Post
     </x-slot>
     <x-category-info :category='$category' />
-    <div class='bg-gray-200 p-4'>
-    <div class='post'>
-        <div class='image'>
-            <img src="{{ $post->post_image }}" alt="画像が読み込めません。"/>
+    <div class='flex-1 bg-gray-200 p-4'>
+        <div class='bg-white w-full shadow-lg p-4 flex flex-col items-center'>
+            <div class='flex justify-between items-center w-full'>
+                <div class='text-3xl'>{{$post->title}}</div>
+                <div class="flex items-center text-gray-700 mb-1 mr-2">
+                    @if (Auth::user()->hasLiked($post))
+                    <!-- いいねを外すボタン -->
+                    <form method="POST" action="{{ route('unlike', $post) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" style="background: none; border: none; padding: 0;">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 fill-current mr-2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                    </form>
+                    @else
+                    <!-- いいねするボタン -->
+                    <form method="POST" action="{{ route('like', $post) }}">
+                        @csrf
+                        <button type="submit" style="background: none; border: none; padding: 0;">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 mr-2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </button>
+                    </form>
+                    @endif
+                    <div class="text-4xl">{{$post->likes->count()}}</div>
+                </div>
+            </div>
+            <div class='x-auto m-4'>
+                <img src="{{ $post->post_image }}" alt="画像が読み込めません。"/>
+            </div>
+        <div class="flex flex-row-reverse w-full items-center">
+            <div class='flex'>
+                @if($post->updated_at!=$post->created_at)
+                <p class="mr-2 uppercase text-sm bg-gray-100 p-0.5 border-gray-500 border rounded text-gray-700 mb-1">更新日時:{{$post->updated_at}}</p>
+                @endif
+                <p class="uppercase text-sm bg-gray-100 p-0.5 border-gray-500 border rounded text-gray-700 mb-1">作成日時:{{$post->created_at}}</p>
+            </div>
         </div>
-        <h2 class='title'>{{$post->title}}</h2>
-        <div class='datetimes'>
-            <p class='updated_at'>更新日時:{{$post->updated_at}}</p>
-            <p class='created_at'>作成日時:{{$post->created_at}}</p>
-        </div>
-        <div class='body'>
+        <div class='mt-4'>
             <p>{{$post->body}}</p>
         </div>
-    </div>
-    <div class='edit'>
+        </div>
+    <div class='flex flex-row-reverse'>
         @if (Auth::id() == $post->user_id)
-        <a href="/posts/{{ $post->id }}/edit">Edit</a>
+        <a href="/posts/{{ $post->id }}/edit" class ="bg-blue-500 text-white mt-2 px-4 py-2 rounded hover:bg-blue-600">Edit</a>
         @endif
     </div>
-    <div class='like'>
-        <p>{{$post->likes->count()}}人がいいねしました</p>
-        @if (Auth::user()->hasLiked($post))
-        <!-- いいねを外すボタン -->
-        <form method="POST" action="{{ route('unlike', $post) }}">
-            @csrf
-            @method('DELETE')
-            <input type="image" src="https://res.cloudinary.com/dig0xnvus/image/upload/v1689676790/Unlike.jpg">
-        </form>
-        @else
-        <!-- いいねするボタン -->
-        <form method="POST" action="{{ route('like', $post) }}">
-            @csrf
-            <input type="image" src="https://res.cloudinary.com/dig0xnvus/image/upload/v1689676791/Like.jpg">
-        </form>
-        @endif
-    </div>
+    
     <div class='comments'>
     <h3>Comments ({{ $comments->count() }})</h3>
     @foreach ($comments as $comment)
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,72 +1,101 @@
 <x-app-layout>
     <x-slot name='header'>
-        Category
+      
     </x-slot>
-    <!-- サイドバー -->
-    <div class="bg-gray-100 w-64 p-4 flex flex-col flex-shrink-0 md:w-50">
-        <div class="sticky top-0 z-10">
-        <!-- 上の文章 -->
-        <div class="text-2xl p-4 mb-4 flex justify-center items-center rounded-full">
-            Following
-        </div>
-    <a href="/">
-    <div class="h-24 bg-white rounded-lg shadow-lg ease-in transition duration-200 transform hover:scale-105 mb-4">
-    <div class="h-12 w-full bg-gray-200 rounded-t-lg flex flex-col justify-between p-4 bg-cover bg-center" style="background-image: url('https://cdn-icons-png.flaticon.com/512/3135/3135715.png')">
-    </div>
-        <div class="flex items-center mb-2 space-x-4 px-2 pt-3 pb-1">
-            <h1 class="text-gray-600">音楽</h1>
-            <p class="text-gray-400 text-sm">@Hanako</p>
-        </div>
-    </div>
-    </a>
-    
-        
-    </div>
-    </div>
-
-    <div class="flex-1 bg-blue-500 p-4">
-    <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
-    @for ($i = 0; $i < 40; $i++)
-    <div class="relative aspect-w-1 aspect-h-1 mb-24">
-        <a href="/">
-            <div class="bg-white rounded-lg shadow-lg ease-in transition duration-200 transform hover:scale-105">
-                <div class="flex items-center mb-2 px-2 pt-3 pb-1">
-                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="説明" class="w-8 h-8 ml-1 mr-4 rounded-full overflow-full hover:opacity-80 ring ring-gray-400">
-                    <p class="text-gray-600">Music</p>
-                    <p class="text-gray-400">-@Hanako</p>
+    <div class='flex-1 bg-gray-200 p-4'>
+        <form action='/posts' method='POST' enctype='multipart/form-data'>
+            @csrf
+            
+            <div class='image'>
+                <div class="flex justify-between items-center">
+                    <input id='file-input' type='file' name='image' accept='image/*'>
+                    <button type='button' id='reset-file' class="px-4 py-2 bg-red-400 text-white rounded mr-4">Reset File</button>
                 </div>
-                <div class="aspect-content overflow-hidden">
-                    <img class="object-cover w-full h-full" src="https://source.unsplash.com/random/200x200?sig={{ $i }}" alt="Image {{ $i }}">
+                <div class="relative bg-white m-4 mx-auto flex justify-center items-center" style="width: 400px; height: 400px;">
+                    <span class="text-center absolute z-0">画像を選択してください</span>
+                    <div id="crop-image-area" class="absolute top-0 left-0 w-full h-full flex justifys-center items-center bg-gray-300 z-10" style="display:none;">
+                        <div id="croppie" class="w-full max-w-screen-sm"></div>
+                        <button type="button" id="crop-button" class="absolute mt-3 px-4 py-2 bg-blue-600 text-white rounded" style="right: -150px;">Crop Image</button>
+                    </div>
+                    <div class="preview-cropped-image flex justify-center items-center h-full z-10"></div>
                 </div>
-                <div class="flex items-center mb-2 space-x-4 px-2 pt-3 pb-1">
-                    <h1 class="text-gray-600">タイトル1</h1>
-                </div>
+                <input type="hidden" name="cropped_image" id="cropped_image">
             </div>
-        </a>
-    </div>
-    
-    <div class="relative aspect-w-1 aspect-h-1 mb-24">
-        <a href="/">
-            <div class="bg-white rounded-lg shadow-lg ease-in transition duration-200 transform hover:scale-105">
-                <div class="flex items-center mb-2 px-2 pt-3 pb-1">
-                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="説明" class="w-8 h-8 ml-1 mr-4 rounded-full overflow-full hover:opacity-80 ring ring-gray-400">
-                    <p class="text-gray-600">Music</p>
-                    <p class="text-gray-400">-@Hanako</p>
-                </div>
-                <div class="aspect-content overflow-hidden">
-                    <img class="object-cover w-full h-full" src="https://res.cloudinary.com/dig0xnvus/image/upload/v1689078150/picture_1.jpg" alt="Image {{ $i }}">
-                </div>
-                <div class="flex items-center mb-2 space-x-4 px-2 pt-3 pb-1">
-                    <h1 class="text-gray-600">タイトル1</h1>
-                </div>
+            <div class="mt-12 mb-8">
+                <label for="title" class="ml-2 text-sm block">タイトル</label>
+                <input id="title" type="text" name="post[title]" class="border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="タイトルを入力">
             </div>
-        </a>
+            <div class="mb-8">
+                <label for="body" class="ml-2 text-sm block">本文</label>
+                <textarea id="body" name="post[body]"  class="h-40 border-0 outline-none mx-2 mt-2 placeholder-gray-500 placeholder-opacity-50 w-full py-2" placeholder="本文を入力"></textarea>
+            </div>
+            <div class="flex justify-end mt-4">
+                <input type='submit' value='投稿' class="px-4 py-2 bg-green-600 text-white rounded">
+                </div>
+        </form>
     </div>
-    @endfor
-</div>
-
-</div>
-    
-    
-    
 </x-app-layout>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js"></script>
+
+<script>
+    const fileInput = document.getElementById('file-input');
+    const cropImageArea = document.getElementById('crop-image-area');
+    const croppieElement = document.getElementById('croppie');
+    const croppedImageInput = document.getElementById('cropped_image');
+    let croppieInstance = null;
+
+    fileInput.addEventListener('change', function() {
+    
+    document.querySelector('.preview-cropped-image').innerHTML = "";
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                if (croppieInstance) {
+                    croppieInstance.destroy();
+                }
+                croppieInstance = new Croppie(croppieElement, {
+                    viewport: {
+                        width: 300,
+                        height: 300,
+                        type: 'square'
+                    },
+                    boundary: {
+                        width: 400,
+                        height: 400
+                    },
+                    enforceBoundary: true
+                });
+                croppieInstance.bind({
+                    url: event.target.result
+                });
+                cropImageArea.style.display = 'block';
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+
+
+    document.getElementById('crop-button').addEventListener('click', function() {
+        croppieInstance.result('base64').then(function(base64) {
+        console.log(base64);
+            croppedImageInput.value = base64; // トリミング後の画像データをフィールドにセット
+            document.querySelector('.preview-cropped-image').innerHTML = `<img src="${base64}" alt="Cropped Image" />`;
+            cropImageArea.style.display = 'none';
+        });
+    });
+
+    document.getElementById('reset-file').addEventListener('click', () => {
+    fileInput.value = "";
+    cropImageArea.style.display = 'none';
+    if (croppieInstance) {
+        croppieInstance.destroy();
+        croppieInstance = null; // 破棄後にnullを代入して明示的にインスタンスをリセット
+    }
+    document.querySelector('.preview-cropped-image').innerHTML = "";
+    croppedImageInput.value = "";
+});
+</script>
