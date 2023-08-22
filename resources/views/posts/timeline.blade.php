a4d75b1e37907e59cdcbe27fb29ef1924a84243b
chronoll <kurotaka.wa@gmail.com>
2023-08-22 16:36:45 +0900

    パンくずリストを修正

--- resources/views/posts/timeline.blade.php
+++ resources/views/posts/timeline.blade.php
@@ -1,6 +1,11 @@
 <x-app-layout>
     <x-slot name='header'>
+        <div class="flex items-center space-x-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+        </svg>
         <span class="text-xl">Timeline</span>
+        </div>
     </x-slot>
     <div class='bg-gray-100 w-64 p-4 flex flex-col flex-shrink-0 md:w-50'>
         <div class="sticky top-0 z-10">
